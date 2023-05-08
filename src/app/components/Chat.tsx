dc97504f3d26db69a6e9ac5879dadb43651edf88
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-08 18:22:59 +0530

    Fix trigger color

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -15,7 +15,7 @@ const Chat = () => {
       <AccordionItem value="item-1">
         <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
           <div className="w-full h-full flex flex-col">
-            <AccordionTrigger className="px-6 border-b border-zinc-300 ">
+            <AccordionTrigger className="px-6 border-b border-zinc-300 text-black">
               <ChatHeader />
             </AccordionTrigger>
           </div>
