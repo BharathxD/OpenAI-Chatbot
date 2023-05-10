6a178452f9e861cb8f32f50097cea84a704703db
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-10 15:56:45 +0530

    Configure loader

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -15,6 +15,7 @@ import TextareaAutosize from "react-textarea-autosize";
 import { sendMessage } from "@/api";
 import { MessagesContext } from "@/context/messages";
 import { Message } from "@/lib/validators/message";
+import { CornerDownLeft, Loader2 } from "lucide-react";
 
 /**
  *  The `interface ChatInputProps` is defining the props that can be passed to the `ChatInput` component.
@@ -89,9 +90,23 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
           maxRows={4}
           placeholder={"Write a message..."}
           value={input}
+          disabled={isLoading}
           onChange={(event) => setInput(event.target.value)}
           className="peer disabled:opacity-50 pr-14 text-black resize-none block w-full border-0 bg-zinc-100 py-1.5 tex-grey-900 focus:ring-0 text-sm sm:leading-6"
           autoFocus
+        />
+        <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+          <kbd className="inline-flex items-center rounded border bg-white border-gray-200 px-1 font-sans text-xs text-gray-400">
+            {isLoading ? (
+              <Loader2 className="w-3 -h-3 animate-spin" />
+            ) : (
+              <CornerDownLeft className="w-3 h-3" />
+            )}
+          </kbd>
+        </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 bottom-0 border-t border-gray-300 peer-focus:border-t-2 peer-focus:border-indigo-600"
         />
       </div>
     </div>
